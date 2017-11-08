d76dd753fb1a11c9cf6dbc11998e7f67b6ece904
tonyligithub <xinr.li@mail.utoronto.ca>
2017-11-08 00:45:30 -0500

    Added demo for NFC Scanning; converts tags of non-NDEF tags into Hexadecimal values and displays it.

--- app/src/main/java/com/wastetracking/wastetracking/MainActivity.java
+++ app/src/main/java/com/wastetracking/wastetracking/MainActivity.java
@@ -1,52 +1,175 @@
 package com.wastetracking.wastetracking;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
+import android.nfc.NfcAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.util.Log;
+
+/**
+ * Activity for reading data from an NDEF Tag.
+ *
+ *
+ */
+public class MainActivity extends Activity {
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        mTextView = (TextView) findViewById(R.id.main_text_view);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText("NFC is active!");
+        }
+
+        handleIntent(getIntent());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        //super.onNewIntent(intent);
+        Log.d(TAG, "GOT TO HERE YO");
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        Log.d(TAG, "Action: " + action);
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Log.d(TAG, "Scan triggered NDEF_DISCOVERED");
+            // TODO: Properly treat when NDEF_DISCOVERED
+
+            String placeholder = "NDEF payload detected! Currently not able to process!";
+            mTextView.setText(placeholder);
+
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            Log.d(TAG, "Scan triggered TECH_DISCOVERED");
+            // TODO: Properly treat when TAG_DISCOVERED
+
+            String placeholder = "NDEF payload detected, but cannot be mapped to a MIME type or URI!";
+            mTextView.setText(placeholder);
+
+        } else if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            Log.d(TAG, "No chip technology found, but there is a tag!");
+
+            byte[] extracted_bytes = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+            String converted_string;
+
+            try {
+                converted_string = Utility.bytesToHex(extracted_bytes);
+            } catch (Exception e) {
+                converted_string = "Failed to convert!";
+            }
+
+            String referenced_value = "Not found in device records!";
+
+            switch (converted_string) {
+
+                case ("04714E72334680"):
+                    referenced_value = "Tony's Presto Card";
+                    break;
+
+                case ("041A2A72BB3380"):
+                    referenced_value = "Expired Presto Card";
+                    break;
+            }
+
+            String concat_string = "Scanned Hexadecimal Value: " + converted_string + "\n"
+                    + "Cross-Referenced Value: " +  referenced_value;
+
+            mTextView.setText(concat_string);
+        }
+    }
+
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
 }
